41763d0ac58256dee0cdeec2899edfef41fba6d4
Joseph-Magee <joseph.magee@kainos.com>
2023-12-05 11:54:22 +0000

    fix

--- testing.js
+++ testing.js
@@ -1,8 +1,9 @@
-let x = 10; // use let or const instead of var
-x += 5; // use += operator instead of reassignment
-console.log("The value of x is " , x); // use template literals instead of string concatenation
+let x = 10 // use let or const instead of var
+x += 5 // use += operator instead of reassignment
+console.log('The value of x is ', x) // use template literals instead of string concatenation
 if (x > 15) // use curly braces for if statements
-    {console.log("x is greater than 15")} // indent the code properly
-else{
-    console.log("x is less than or equal to 15")};
-console.log("This is a short line"); // avoid lines longer than 80 characters
+{ console.log('x is greater than 15') } // indent the code properly
+else {
+  console.log('x is less than or equal to 15')
+};
+console.log('This is a short line') // avoid lines longer than 80 characters
