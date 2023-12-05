a085e50041f32ec33b6f50812e8543e994480b17
Joseph-Magee <joseph.magee@kainos.com>
2023-12-05 12:31:29 +0000

    acommit

---
+++ testing.js
@@ -0,0 +1,8 @@
+var x = 10 // use let or const instead of var
+x = x + 5 // use += operator instead of reassignment
+console.log('The value of x is ' + x) // use template literals instead of string concatenation
+if (x > 15) 
+console.log('x is greater than 15')
+else 
+console.log('x is less than or equal to 15')
+console.log('This is a realy realy  realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy realy long line') // avoid lines longer than 80 characters
