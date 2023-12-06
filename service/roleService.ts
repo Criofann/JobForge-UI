6a47bf9023a1c5684beca5045fda2b8d34b28acd
Odhran <odhran.hague@kainos.com>
2023-12-06 10:07:58 +0000

    view roles

---
+++ service/roleService.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+module.exports.getAllRoles = async function() {
+    try{
+        const response = await axios.get('http://localhost:8080/api/job-roles')
+
+        return response.data
+    } catch(e) {
+        console.error("get request failed for api/job-roles");
+        return new Error('Could not get orders')
+    }
+}
