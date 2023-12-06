4a626f4fc22b521bb10ab7c1563657ae6f757378
Odhran <odhran.hague@kainos.com>
2023-12-06 09:44:43 +0000

    model for role class

---
+++ controller/roleController.ts
@@ -0,0 +1,7 @@
+import { Application } from "express";
+
+
+
+module.exports = function(app: Application){
+    
+}
---
+++ model/role.ts
@@ -0,0 +1,8 @@
+export class Role{
+    roleName: String
+    specification: String
+    capabilityName?: String
+    bandName?: String
+    Responsibilities: String
+    sharepointLink: String
+}
