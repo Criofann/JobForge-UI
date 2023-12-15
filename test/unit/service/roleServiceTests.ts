88ac86bd1e4645259422510f877cc817d9382d16
Odhran <odhran.hague@kainos.com>
2023-12-15 09:59:35 +0000

    stashing for later

--- test/unit/service/roleServiceTests.ts
+++ test/unit/service/roleServiceTests.ts
@@ -1,8 +1,10 @@
-var axios = require('axios');
-var MockAdapter = require('axios-mock-adapter');
-var chai = require('chai');  
-const expect = chai.expect;
-const RoleService = require("../../../service/roleService");
+import axios from "axios"
+import MockAdapter from "axios-mock-adapter"
+import chai from "chai" 
+import expect from "chai.expect"
+const expect1 = chai.expect;
+var RoleService = require("../../../service/roleService");
+
 
 const role = {
     roleName : "role",
@@ -23,7 +25,7 @@ describe("RoleService", function () {
 
             var results = await RoleService.getRoles()
 
-            expect(results[0]).to.deep.equal(role)
+            expect1(results[0]).to.deep.equal(role)
         })
     })
 })
