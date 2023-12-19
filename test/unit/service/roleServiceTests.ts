23f46b95d083cb2b41709105b410a4711d26a497
Odhran <odhran.hague@kainos.com>
2023-12-19 14:24:38 +0000

    Added real info for test case

--- test/unit/service/roleServiceTests.ts
+++ test/unit/service/roleServiceTests.ts
@@ -4,10 +4,18 @@ import { expect } from "chai";
 const roleService = require("../../../service/roleService");
 
 const role = {
-    roleName : "role",
-    specification : "ijasndjsanknajn spec entered bla bla",
+    roleName : "AP Software Engineer",
+    specification : "As an Apprentice Software Engineer with Kainos, you will work on projects where you can"
+        + "make a real difference to people’s lives – the lives of people you know. extensive training"
+        + "to set you off on the right foot, you will quickly work as a part of a team in developing"
+        + "solutions within our real projects, learning all about our development languages, projects"
+        + "and technologies. You will be fully supported by experienced colleagues in the team as"
+        + "well as an experienced mentor, who will provide training and mentoring throughout your"
+        + "studies. You’ll also get experience across a wide range of teams and projects, with built-in"
+        + "rotations to help you learn and work out which element of Software Engineering suits your"
+        + "interests and skills best. ",
     capabilityName : "engineering",
-    responsibilities : "be very responsible",
+    responsibilities : "team-working, problem solving",
     sharepointLink : "https://kainossoftwareltd.sharepoint.com/SitePages/Home.aspx",
 };
 
