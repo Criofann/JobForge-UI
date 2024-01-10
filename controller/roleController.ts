98d43f2c46421af4f388a8022de1308b71719ca8
criofann <criofan.mccarneysavage@kainos.com>
2024-01-10 15:43:31 +0000

    Fixing lint errors

--- controller/roleController.ts
+++ controller/roleController.ts
@@ -1,10 +1,8 @@
 import { Role } from "../model/role";
-import { Band } from "../model/band";
 import { BandRole } from "../model/bandRole";
 import { Application, Request, Response } from "express";
 
 const roleService = require("../service/roleService");
-const bandService = require("../service/bandService");
 
 module.exports = function(app: Application){
 
