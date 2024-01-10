191387c17f06f0590573f8e5de9dad3b1555bd82
Odhran <odhran.hague@kainos.com>
2024-01-10 14:40:19 +0000

    only ticket relevant files

--- controller/roleController.ts
+++ controller/roleController.ts
@@ -1,21 +1,17 @@
 import { Role } from "../model/Role";
-import { Band } from "../model/band";
 import { Application, Request, Response } from "express";
 
 const roleService = require("../service/roleService");
-const bandService = require("../service/bandService");
 
 module.exports = function(app: Application){
     app.get("/job-roles", async (req: Request, res: Response) => {
         let data: Role[];
-        let dataa: Band[];
         try{
             data = await roleService.getAllRoles();
-            dataa = await bandService.getAllBands();
         } catch (e) {
             console.log(e);
         }
-        res.render("roles", { roles : data, bands : dataa});
+        res.render("roles", { roles : data });
     });
 
     app.get("/job-role-spec/:roleName", async (req: Request, res: Response) => {
--- service/roleService.ts
+++ service/roleService.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { Role } from "../model/Role";
-const roleValidator = require("../validator/roleValidator");
 axios.defaults.baseURL = "http://localhost:8080";
 
 module.exports.URL = "/api/job-roles";
@@ -21,20 +20,6 @@ module.exports.getRoleByID = async function (roleName: string): Promise<Role> {
         return response.data;
     } catch (e) {
         throw new Error("Could not get roles");
-    }
-};
-
-module.exports.createRole = async function(role: Role): Promise<number> {
-    const error: string = roleValidator.validateRole(role);
-    if (error){
-        throw new Error(error);
-    }
-        
-    try{
-        const response = await axios.post("http://localhost:8080/api/job-roles/", role);
-        return response.data;
-    } catch (e) {
-        throw new Error("Could not create orders");
     }
 };
     
