3e5a9504328bba178bc7f333261c597830900df7
Odhran <odhran.hague@kainos.com>
2024-01-11 14:07:03 +0000

    reviewed changes

--- app.ts
+++ app.ts
@@ -11,8 +11,6 @@ app.use("/public", express.static(path.join(__dirname,"public")));
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: false}));
-
 const appViews = path.join(__dirname, "/views/");
 
 const nunjucksConfig = {
@@ -28,7 +26,7 @@ app.set("view engine", "html");
 app.use(express.json());
 app.use(express.urlencoded({extended : true }));
 
-app.use(session({ secret: "NOT HARDCODED SECRET", cookie: {maxAge: 60000}}));
+app.use(session({ secret: process.env.SESSION_SECRET, cookie: {maxAge: 60000}}));
 
 declare module "express-session" {
     interface SessionData {
@@ -46,3 +44,4 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 require("./controller/authController")(app);
+require("./controller/roleController")(app);
--- service/authService.ts
+++ service/authService.ts
@@ -1,12 +1,12 @@
+import axios from "axios";
 import { Login } from "../model/Login";
 
-const axios = require("axios");
 axios.defaults.baseURL = "http://localhost:8080";
-module.exports.URL = "/api/login";
+module.exports.URL = "/api/login/";
 
 export async function login(login: Login): Promise<void>{
     try{
-        const response = await axios.post("http://localhost:8080/api/login", login);
+        const response = await axios.post(this.URL, login);
     
         return response.data;
     } catch(e) {
