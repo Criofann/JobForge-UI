70d95fce6609345dcbb0f727927d515df9a57d4c
criofann <criofan.mccarneysavage@kainos.com>
2024-01-11 11:45:07 +0000

    Adding some bug fixes

--- service/roleService.ts
+++ service/roleService.ts
@@ -7,7 +7,7 @@ module.exports.URL = "/api/job-roles";
 
 module.exports.getAllRoles = async function() {
     try{
-        const response = await axios.get("http://localhost:8080/api/job-roles/");
+        const response = await axios.get(this.URL);
         return response.data;
     } catch(e) {
         throw new Error("Could not get roles");
